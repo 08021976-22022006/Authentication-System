4abe8a899c2f94510b7836df03c1c862819f73bb
08021976-22022006 <poojanas.dev@gmail.com>
2025-09-05 12:31:31 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config';
+import cookieparser from "cookie-parser"
+import connectDB from './config/mongodb.js';
+
+
+const app = express();
+const port = process.env.PORT || 4000
+
+connectDB();
+
+app.use(express.json());
+app.use(cookieparser());
+app.use(cors({credentials: true}))
+
+
+
+app.get('/', (req, res)=>res.send("API Working"));
+
+app.listen(port, ()=>{
+    console.log(`Server started in PORT: ${port}`)
+});
